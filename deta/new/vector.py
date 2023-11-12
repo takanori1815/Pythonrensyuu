2be1ff979497023cfe4d238ef385566d22dd7884
takanori <takakki2810@gmail.com>
2023-11-12 16:04:04 +0900

    try

---
+++ deta/new/vector.py
@@ -0,0 +1,10 @@
+from typing import list
+
+Vector = List[float]
+height_weight_age = [70,#
+                     170,
+                     40]
+grades = [95,
+          80,
+          75,
+          62]
